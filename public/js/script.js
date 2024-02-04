aedd95e2724282378d3ad3529a54667c08de6493
BlackKnight683 <75422863+Aayush-683@users.noreply.github.com>
2024-02-04 21:14:13 +0530

    Added js

---
+++ public/js/script.js
@@ -0,0 +1,60 @@
+const answers = [
+    "Are you sure?",
+    "Are you really sure??",
+    "Are you really realy sure???",
+    "Think again?",
+    "Don't believe in second chances?",
+    "Why are you being so cold?",
+    "Maybe we can talk about it?",
+    "I am not going to ask again!",
+    "Ok now this is hurting my feelings!",
+    "You are now just being mean!",
+    "Why are you doing this to me?",
+    "Please give me a chance!",
+    "I am begging you to stop!",
+    "Ok, Lets just start over.."
+]
+
+const no_button = document.getElementById('no-button');
+const yes_button = document.getElementById('yes-button');
+let i = 0;
+let size = 50;
+let clicks = 0;
+
+no_button.addEventListener('click', () => {
+    // Change banner source
+    let banner = document.getElementById('banner');
+    if (clicks === 0) banner.src = "images/no.gif";
+    clicks++;
+    // increase button height and width gradually to 250px
+    const sizes = [40, 50, 30, 35, 45]
+    const random = Math.floor(Math.random() * sizes.length);
+    size += sizes[random]
+    yes_button.style.height = `${size}px`;
+    yes_button.style.width = `${size}px`;
+    let total = answers.length;
+    // change button text
+    if (i < total - 1) {
+        no_button.innerHTML = answers[i];
+        i++;
+    } else if (i === total - 1) {
+        alert(answers[i]);
+        i = 0;
+        no_button.innerHTML = "No";
+        yes_button.style.height = "50px";
+        yes_button.style.width = "50px";
+        size = 50;
+    }
+});
+
+yes_button.addEventListener('click', () => {
+    // change banner gif path
+    let banner = document.getElementById('banner');
+    banner.src = "images/yes.gif";
+    // hide buttons div
+    let buttons = document.getElementsByClassName('buttons')[0];
+    buttons.style.display = "none";
+    // show message div
+    let message = document.getElementsByClassName('message')[0];
+    message.style.display = "block";
+});
